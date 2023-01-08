b68230d4f046325da51ddcba7b70ca7c895b18b2
Kha Tran <khatran9123@gmail.com>
2023-01-08 00:37:30 -0600

    weather app using react

---
+++ Documents/react-weather-app/weather-app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return <div>
+    <div className='flex items-center justify-center my-6'>
+        <p className='text-white text-xl font-extralight'>
+            Tuesday, 31 November 2022 | Local Time: 12:41 PM
+        </p>
+    </div>
+    <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium'>
+            Omaha, NE
+        </p>
+    </div>
+    <div></div>
+  </div>
+  
+}
+
+export default TimeAndLocation
